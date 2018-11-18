63602d47fc40db5dee7deb0df55047533c75518d
PeaStew <34198053+PeaStew@users.noreply.github.com>
2018-11-18 04:58:50 +0100

    To support https://github.com/MONKEYPROJECT/Guides/pull/1 and install jq

--- monk_setupv2.sh
+++ monk_setupv2.sh
@@ -33,6 +33,7 @@ if [[ ${DOSETUP,,} =~ "y" ]] ; then
   sudo apt-get update
   sudo apt-get install -y libdb4.8-dev libdb4.8++-dev
   sudo apt-get install -y dos2unix
+  sudo apt-get install -y jq
 
   cd /var
   sudo touch swap.img
